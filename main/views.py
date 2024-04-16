3bf377f4eac1682832cd0bffdc1ec8911b6f5653
pasik <parasatto@gmail.com>
2024-04-16 16:14:33 +0500

    the main app is ready home and about pages

--- main/views.py
+++ main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'Home - Главная',
+        'content': "Магазин мебели HOME"
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'Home - О нас',
+        'content': "О нас",
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар."
+    }
+
+    return render(request, 'main/about.html', context)
